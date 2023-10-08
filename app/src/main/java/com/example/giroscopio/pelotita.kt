293eceaab98051961bdea7659a6e2451c63e112e
BryanChrisRH <sneakyrdz501.441@gmail.com>
2023-10-08 15:11:08 -0600

    Pelotica

--- app/src/main/java/com/example/giroscopio/pelotita.kt
+++ app/src/main/java/com/example/giroscopio/pelotita.kt
@@ -1,6 +1,6 @@
 package com.example.giroscopio
 
-import  android.content.Context
+import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
